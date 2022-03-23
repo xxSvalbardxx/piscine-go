82f156eb5eb8526c8db40816dfee13050b97e9ce
Svalbard <svalbardmalfant@gmail.com>
2022-03-23 10:15:33 +0100

    jq example 2

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select(.id == 1  ) | .connections |.relatives' | sed 's/"//g'
+curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select(.id == '$HERO_ID' ) | .connections.relatives' | sed 's/"//g'
