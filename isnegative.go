607a3c913be2100911bbce656e424ad30c3d42cc
Svalbard <svalbardmalfant@gmail.com>
2022-03-24 12:33:46 +0100

    negate

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func isNegative(nb int) {
-	if nb >= 0 {
-		z01.PrintRune('F')
-	} else {
+	if nb < 0 {
 		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
