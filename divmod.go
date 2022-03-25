5eb5fa5cfe6bfea0aab784023704af89e6036d4d
Svalbard <svalbardmalfant@gmail.com>
2022-03-25 10:30:25 +0100

    division

--- divmod.go
+++ divmod.go
@@ -1,17 +1,12 @@
 package piscine
 
-import (
-	"fmt"
-
-	"piscine"
-)
-
 func DivMod(a int, b int, div *int, mod *int) {
 	a := 13
 	b := 2
-	var div int
-	var mod int
-	piscine.DivMod(a, b, &div, &mod)
-	fmt.Println(div)
-	fmt.Println(mod)
-}
+	
+	division := a / b
+
+	a := 13
+	b := 2
+
+	modulo := a % b
