fe20fc24f1cc6cfe564056aeff52fde71acf659d
Svalbard <svalbardmalfant@gmail.com>
2022-03-27 15:39:29 +0200

    printstr

--- printstr.go
+++ printstr.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func Printstr(s string) {
 	s = "Hello World!"
 	for s := 0; s <= 12; s++ {
-		z01.PrintRune(rune(s))
+		z01.PrintRune(rune("s"))
 		z01.PrintRune('\n')
 	}
 }
