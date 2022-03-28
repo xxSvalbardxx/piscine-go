da796bcea75a5ba8bf1e062b7c4acf994ad66cd5
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 16:01:01 +0200

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,7 +5,7 @@ func RecursiveFactorial(nb int) int {
 		return 1
 	} else if nb < 0 {
 		return 0
-	} else if nb > 1 {
+	} else if nb > 1 && nb > 2147483647 {
 		return nb * RecursiveFactorial(nb-1)
 	}
 }
