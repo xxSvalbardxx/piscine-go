e1f3d578c9aff2be3561b86fb683189f09635d8b
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 13:54:16 +0200

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,9 +3,10 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
-	} else if nb == 1 || nb == 0 {
+	} else if nb = 1 || nb = 0 {
 		return 1
-	} else if nb == nb*(nb-1) {
+	} else {
+	for nb = nb*(nb-1) {
 		return nb
 	}
 }
