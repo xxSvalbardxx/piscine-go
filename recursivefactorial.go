effa28ed706b78aef233944bb4d75e3a0f3fa4c8
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 16:29:47 +0200

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 0 || nb == 1 {
-		return 1
-	} else if nb < 0 {
-		return 0
-	} else if nb > 1 && nb <= 2147483647 {
+	if nb > 1 && nb <= 2147483647 {
 		return nb * RecursiveFactorial(nb-1)
+	} else if nb >= 0 && nb <= 1 {
+		return 1
+	} else {
+		return 0
 	}
 }
