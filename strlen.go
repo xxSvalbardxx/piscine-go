031cad533cc30bf288aaa13200d8d274aa34dc59
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 08:32:37 +0200

    string length

--- strlen.go
+++ strlen.go
@@ -3,6 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func StrLen(s string) int {
-	for i := range(s)
-	z01.PrintRune(rune(s[i]))
+	for i := range s {
+		z01.PrintRune(rune(s[i]))
+	}
 }
