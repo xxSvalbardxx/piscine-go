55638c34ae7576c0302d8a381efac828f31ffef9
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 07:50:06 +0200

    print a string

--- printstr.go
+++ printstr.go
@@ -3,13 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	s = "Hello World!"
-	for s {
-		i := 0
-		i <= 11
-		i++
-	}
-	{
+	for i := 0; i <= len(s)-1; i++ {
 		z01.PrintRune(rune(s[i]))
 		z01.PrintRune('\n')
 	}
