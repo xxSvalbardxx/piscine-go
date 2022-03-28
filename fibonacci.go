42efd6a7930231161c46accdfb917778ad835747
Svalbard <svalbardmalfant@gmail.com>
2022-03-28 18:02:12 +0200

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -3,8 +3,9 @@ package piscine
 func Fibonacci(index int) int {
 	if index == 0 {
 		return 0
-	}else if n == 1 {
+	} else if index == 1 {
 		return 1
 	} else {
-		return index(n - 1) + index(n - 2)
+		return Fibonacci(index-1) + (index - 2)
+	}
 }
