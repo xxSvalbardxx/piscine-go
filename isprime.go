0313e6b5b4c58997935029d5ded9e4aaa592cd17
Svalbard <svalbardmalfant@gmail.com>
2022-03-29 10:31:56 +0200

    primary nbr

--- isprime.go
+++ isprime.go
@@ -5,15 +5,15 @@ func IsPrime(nb int) bool {
 	if nb == 0 || nb == 1 {
 		return false
 	} else {
-		for i := 2 ; i <= nb / 2 ;i++ {
+		for i := 2; i <= nb/2; i++ {
 			if nb%i == 0 {
 				return false
 				rslt = false
 				break
 			}
 		}
-			if rslt == true
-				return true
+		if rslt == true {
+			return true
+		}
 	}
 }
-
