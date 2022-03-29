94b9ab4bb93e7681def7dfbce03f84e374882df0
Svalbard <svalbardmalfant@gmail.com>
2022-03-29 10:31:03 +0200

    primary nbr

--- isprime.go
+++ isprime.go
@@ -1,10 +1,19 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	for (let i = 2; i * i <= nb; i++) {
-		if (nb % i == 0) {
-	  	return false 
-	  	}
+	rslt := true
+	if nb == 0 || nb == 1 {
+		return false
+	} else {
+		for i := 2 ; i <= nb / 2 ;i++ {
+			if nb%i == 0 {
+				return false
+				rslt = false
+				break
+			}
+		}
+			if rslt == true
+				return true
 	}
-	return nb > 1;
 }
+
