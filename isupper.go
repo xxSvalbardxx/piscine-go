83470eb506063e9b41473f4d876a8fec1bd90e12
Svalbard <svalbardmalfant@gmail.com>
2022-03-30 17:20:17 +0200

    detect Maj

--- isupper.go
+++ isupper.go
@@ -1,15 +1,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	x := 0
 	str := []rune(s)
 	for i := range str {
 		if (str[i] < 'A') && (str[i] > 'Z') {
-			x += 1
-			if x > 0 {
-				return false
-			}
-			continue
+			return false
 		}
 	}
 	return true
