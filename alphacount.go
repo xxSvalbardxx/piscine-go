8c6631d71b80c7f234e32afe8c62935c0b803d3c
Svalbard <svalbardmalfant@gmail.com>
2022-03-30 09:51:18 +0200

    string counter

--- alphacount.go
+++ alphacount.go
@@ -1,13 +1,12 @@
 package piscine
 
-
-
 func AlphaCount(s string) int {
 	x := 0
 	str := []rune(s)
-	for i := 0 ; i <= len(s)-1 ; i++ {
-		if (str[i] >= 'A') && (str[i] <= 'Z') || (str[i] > 'a') && (str[i] < 'z')
-			x = x +1
+	for i := 0; i <= len(s)-1; i++ {
+		if (str[i] >= 'A') && (str[i] <= 'Z') || (str[i] > 'a') && (str[i] < 'z') {
+			x = x + 1
+		}
 	}
 	return x
 }
