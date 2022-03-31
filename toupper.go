14cda2c3d294b260d141246a7a88223cf7ce51f9
Svalbard <svalbardmalfant@gmail.com>
2022-03-31 09:42:37 +0200

    replace min by MAJ

--- toupper.go
+++ toupper.go
@@ -1,11 +1,11 @@
 package piscine
 
-func ToUpper(s string) bool {
+func ToUpper(s string) string {
 	str := []rune(s)
 	for i := 0; i < len(str); i++ {
 		if (str[i] >= 'a') && (str[i] <= 'z') {
 			str[i] = str[i] - 32
 		}
 	}
-	return false
+	return s
 }
