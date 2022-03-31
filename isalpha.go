f55f347224a96643551051209bf2f258da97d01c
Svalbard <svalbardmalfant@gmail.com>
2022-03-31 07:50:03 +0200

    detect alphanumerique

--- isalpha.go
+++ isalpha.go
@@ -3,11 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	str := []rune(s)
 	for i := 0; i < len(str); i++ {
-		if (str[i] < 'A') || (str[i] > 'Z') {
-			return false
-		} else if (str[i] < 'a') || (str[i] > 'z') {
-			return false
-		} else if (str[i] < '0') || (str[i] > '9') {
+		if (str[i] < '0') || (str[i] > '9') && (str[i] < 'A') || (str[i] > 'Z') && (str[i] < 'a') || (str[i] > 'z') {
 			return false
 		}
 	return true
