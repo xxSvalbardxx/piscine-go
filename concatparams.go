4f6c62d6ec5d0849ebf4662f554a3d132268e25a
Svalbard <svalbardmalfant@gmail.com>
2022-04-02 12:52:17 +0200

    Args return string

--- concatparams.go
+++ concatparams.go
@@ -4,7 +4,7 @@ func ConcatParams(args []string) string {
 	res := "" // Résultat est égal à un string vide (sale)
 
 	for _, val := range args { // Pour la valeur et non l'index dans ce que contient args
-		res += val // resultat est égal à retour à la ligne + valeur
+		res += "\n" + val // resultat est égal à retour à la ligne + valeur
 	}
 	return res[1:] // On retourne le résultat, de l'index 1 (: permet d'appeler un string)
 }
