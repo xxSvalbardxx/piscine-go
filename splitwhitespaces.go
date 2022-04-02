f429c23682e3d8be9206177492b9282f1c28d72d
Svalbard <svalbardmalfant@gmail.com>
2022-04-02 15:12:10 +0200

    decouper une string en plusieurs arguments

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,23 +1,17 @@
-package main
-
-import "fmt"
+package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	var printstr []string
+	var printstr []string // creer la nouvelle variable
 	j := 0
 	for i := 0; i < len(s)-1; i++ {
-		if rune(s[i]) == ' ' {
-			if i != j {
+		if rune(s[i]) == ' ' { // a chaque fois que la boucle tombe sur un espace
+			if i != j { //
 				printstr = append(printstr, s[j:i])
 			}
-			j = j
+			j = i + 1 // permet d'eviter la repetition du mot precedent
 		} else if i == len(s)-4 { //-1 / - 4 permet d'avoir toute la Phrase
 			printstr = append(printstr, s[j:])
 		}
 	}
 	return printstr
-}
-
-func main() {
-	fmt.Printf("%#v\n", SplitWhiteSpaces("Hello how are you?"))
 }
