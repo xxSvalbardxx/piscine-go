1afddfe58915771577a5db0b3d6b1928d879f9e8
Svalbard <svalbardmalfant@gmail.com>
2022-04-02 12:51:07 +0200

    Args return string

---
+++ concatparams.go
@@ -0,0 +1,10 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	res := "" // Résultat est égal à un string vide (sale)
+
+	for _, val := range args { // Pour la valeur et non l'index dans ce que contient args
+		res += val // resultat est égal à retour à la ligne + valeur
+	}
+	return res[1:] // On retourne le résultat, de l'index 1 (: permet d'appeler un string)
+}
