e7e4ab250cbdbb7a5299ba4b4204fd3c3bb8f2d3
Svalbard <svalbardmalfant@gmail.com>
2022-04-04 12:38:01 +0200

    Boolean Args

---
+++ boolean/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	arg := os.Args[1:]
+	if isEven(len(arg)) {
+		printStr("I have an even number of arguments")
+	} else {
+		printStr("I have an odd number of arguments")
+	}
+}
