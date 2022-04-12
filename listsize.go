6ed0bd4297f7d8c27dd8275362d0ff66f44611cf
Svalbard <svalbardmalfant@gmail.com>
2022-04-12 17:43:40 +0200

    how many nodes

--- listsize.go
+++ listsize.go
@@ -1,17 +1,47 @@
 package piscine
 
-func ListSize(l *List) int {
-	n := &NodeL{}
-	count := 0
-	if l.Head == nil {
-		l.Head = n
-		count++
+/*import (
+	"fmt"
+)
+
+type NodeL struct {
+	Data interface{} // is an element of the node
+	Next *NodeL      // indicate where/what is the next node
+}
+
+type List struct {
+	Head *NodeL // the only node that we use and modify here
+	Tail *NodeL // completely useless here because of the main func
+}
+
+func ListPushFront(l *List, data interface{}) {
+	end := &NodeL{Data: data} //  on initialise un noeud et on met nos données à l'intérieur {....}
+	if l.Head == nil {        // l.head = first node
+		l.Head = end
 	} else {
-		current := l.Head
-		for current.Next != nil {
-			current = current.Next
-			count++
-		}
+		end.Next = l.Head // on dit que la variable pour passer au suivant devient le premier noeud
+		l.Head = end      // the first node become the last
+	}
+}*/
+
+func ListSize(l *List) int {
+	count := 0
+	for l.Head != nil {
+		count++
+		l.Head = l.Head.Next // le suivant remplace le precedent
 	}
 	return count
 }
+
+/*
+func main() {
+	link := &List{}
+
+	ListPushFront(link, "Hello")
+	ListPushFront(link, "2")
+	ListPushFront(link, "you")
+	ListPushFront(link, "man")
+
+	fmt.Println(ListSize(link))
+}
+*/
